78fdfbb791df5522d7f1883f50d66e8e08c264cf
Shyamsundar Subramani <shyams0162@gmail.com>
2025-05-02 23:55:28 +0530

    Create SEO.js

---
+++ client/src/components/SEO.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const SEO = ({ title, description, url }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta name="twitter:card" content="summary_large_image" />
+    </Helmet>
+  );
+};
+
+export default SEO;
